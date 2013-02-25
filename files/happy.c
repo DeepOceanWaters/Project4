95e206503c3e84ff777da2813f5bc89854802079
Colin Bradford <cjthehippo@gmail.com>
2013-02-25 13:48:20 -0800

    Initializing files

---
+++ files/happy.c
@@ -0,0 +1,31 @@
+
+/*__________________________HEADER STUFF______________________________________*/
+
+// includes...
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// defines...
+
+// functions...
+
+
+/*_________________________END HEADER STUFF___________________________________*/
+
+
+
+int main(int argc, char *argv[])
+{
+	/* rien */
+	return 0;
+}
+
+
+
+	      /* This white space is used for viewing pleasure */
+
+
+
+/*_____________________________END OF FILE____________________________________*/
