0cc76f0a9a6352c616c6afc3e310bf2ff74d48bd
Colin Bradford <cjthehippo@gmail.com>
2013-02-25 21:14:03 -0800

    Added the OR operator part to the bitmap spike test.

--- files/s_bitmap.c
+++ files/s_bitmap.c
@@ -8,13 +8,27 @@ int main (int argc, const char * argv[])
 	unsigned char c;
 	int i;
 	
+	/* test AND operator */
+	printf("Testing AND (&) operator\n");
 	a = 85; // 01010101
 	b = 1;
 	
 	for(i = 0; i < 8; i++){
-	    c = a & b;
-	    printf("c: %u <---> b: %u\n", c, b);
-	    b = b << 1;
+		c = a & b;
+		printf("c: %u <---> b: %u\n", c, b);
+		b = b << 1;
+	}
+	
+	/* test OR operator */
+	printf("Testing OR (|) operator\n");
+	a = 0; // 0
+	b = 1; // 1
+	
+	for(i = 0; i < 8; i++){
+		printf("Adding %u...\n", b);
+		a = a | b;
+		printf("a: %u <---> b: %u\n", a, b);
+		b = b << 1;
 	}
 	
 	return 0;
